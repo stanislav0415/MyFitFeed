33ac632e8bf74e307c35f961f020fc806db4a674
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-20 22:26:48 +0300

    updated home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer, Header } from './shared/components';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,10 @@ import { Footer, Header } from './shared/components';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App implements OnInit {
+  title = 'web-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
