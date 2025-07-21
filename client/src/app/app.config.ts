2ecdb23e0be3a5ad8493e5ac68acad3b4fa18046
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-21 20:41:11 +0300

    fix warning

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http'; 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,6 +8,5 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-  ]
+    provideHttpClient(withFetch()),]   
 };
