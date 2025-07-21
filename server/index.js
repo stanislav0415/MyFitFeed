604912c80f8cc4b16e183d72c895a198aedd69b8
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-21 18:07:35 +0300

    adding cookie parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import initDatabase from './config/dbConfig.js';
+import cookieParser from 'cookie-parser';
+import { auth } from './middlewares/authMiddleware.js';
+import routes from './routes.js';
 
 const app = express();
 
@@ -9,7 +12,8 @@ await initDatabase();
 
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(express.json());
-
-
+app.use(cookieParser());
+app.use(auth);
+app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on http://localhost:3000'));
