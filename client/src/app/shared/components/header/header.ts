c3dfe83c6ad19a68e2840426d4655b72c067fea4
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-24 16:06:40 +0300

    made header responsive

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +9,15 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './header.css'
 })
 export class Header {
+  protected authService = inject(AuthService);
+  private router = inject(Router);
 
+  readonly isLoggedIn = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+
+  logout(): void {
+   this.authService.logout();
+    this.router.navigate(['/home']);
+  }
 }
