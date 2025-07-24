6949acdf40ef7aa9ecfb90a1b196faf9450d5695
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-24 12:14:15 +0300

    fixed register

--- client/src/app/core/services/auth.service.ts
+++ client/src/app/core/services/auth.service.ts
@@ -1,98 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { User } from '../../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly apiUrl = 'http://localhost:3000/users';
-   private readonly fullUrl = 'http://localhost:3000/users/register';
+
+  private readonly registerUrl = 'http://localhost:3000/users/register';
   private _isLoggedIn = signal<boolean>(false);
   private _currentUser = signal<User | null>(null);
 
   public isLoggedIn = this._isLoggedIn.asReadonly();
   public currentUser = this._currentUser.asReadonly();
 
-  constructor(private http: HttpClient) {
-    const savedUser = localStorage.getItem('currentUser');
-    if (savedUser) {
-      const user = JSON.parse(savedUser);
-      this._currentUser.set(user);
-      this._isLoggedIn.set(true);
-    }
-  }
+  constructor(private httpClient: HttpClient) { }
 
-  login(email: string, password: string): Observable<User> {
-    return new Observable(observer => {
-      this.http.post<{ user: User }>(
-        `${this.apiUrl}/login`,
-        { email, password },
-        { withCredentials: true } 
-      ).subscribe({
-        next: (response) => {
-          this._currentUser.set(response.user);
-          this._isLoggedIn.set(true);
-          localStorage.setItem('currentUser', JSON.stringify(response.user));
-          observer.next(response.user);
-          observer.complete();
-        },
-        error: (err) => {
-          this._isLoggedIn.set(false);
-          observer.error(err);
-        }
-      });
-    });
-  }
-  register(name: string, email: string, password: string, rePassword: string): Observable<boolean> {
-  return new Observable<boolean>(observer => {
-    this.http.post<{ user: User }>(
-      this.fullUrl,
-      { name, email, password, rePassword },
-      { withCredentials: true }
-
-    ).subscribe({
-      next: (response) => {
-        this._currentUser.set(response.user);
-        this._isLoggedIn.set(true);
-        localStorage.setItem('currentUser', JSON.stringify(response.user));
-        observer.next(true);
-        observer.complete();
-      },
-      error: (err) => {
-         console.error('Request URL:', `${this.apiUrl}/register`);  
-        console.error('Registration HTTP Error:', err);      
-        this._isLoggedIn.set(false);
-        observer.error(err);  
+  register(name: string, email: string, password: string, rePassword: string): Observable<User> {
+    const body = JSON.stringify({ name, email, password, rePassword });
+  
+    return this.httpClient.post<User>(this.registerUrl, body, {
+      headers: {
+        'Content-Type': 'application/json'
       }
-    });
-  });
-}
-
-
-  logout(): Observable<void> {
-    return new Observable(observer => {
-      this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).subscribe({
-        next: () => {
-          this._currentUser.set(null);
-          this._isLoggedIn.set(false);
-          localStorage.removeItem('currentUser');
-          observer.next();
-          observer.complete();
-        },
-        error: (err) => {
-          observer.error(err);
-        }
-      });
-    });
+    }).pipe(
+      tap((response) => {
+        localStorage.setItem('auth_token', response.token);
+        this._currentUser.set({ name, email, token: response.token });
+        this._isLoggedIn.set(true);
+      })
+    );
   }
+
 
   getCurrentUser(): User | null {
     return this._currentUser();
-  }
-
-  getCurrentUserId(): string | null {
-    return this._currentUser()?.id || null;
   }
 }
--- client/src/app/features/auth/register/register.ts
+++ client/src/app/features/auth/register/register.ts
@@ -1,133 +1,168 @@
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements AfterViewInit {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
-  name: string = '';
-  email: string = '';
-  password: string = '';
-  rePassword: string = '';
-
-  nameError: boolean = false;
-  emailError: boolean = false;
-  passwordError: boolean = false;
-  rePasswordError: boolean = false;
-
-  nameErrorMessage: string = '';
-  emailErrorMessage: string = '';
-  passwordErrorMessage: string = '';
-  rePasswordErrorMessage: string = '';
-
-  validateName(): void {
-    if(!this.name){
-      this.nameError = true;
-      this.nameErrorMessage = 'Name is required!';
-    } else {
-      this.nameError = false;
-      this.nameErrorMessage = '';
-    }
+  registerForm: FormGroup;
+  serverErrorMessage: string | null = null;
+  constructor() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.pattern(/^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/)]],
+      phone: [''],
+      passwords: this.formBuilder.group({
+        password: ['', [Validators.required, Validators.minLength(5), Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+        rePassword: ['', [Validators.required, this.passwordMatchValidator]],
+      })
+    })
   }
 
-  validateEmail(): void {
-    if(!this.email){
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is required!';
-    } else if (!this.isValidEmail(this.email)) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is not valid!';
-    } else {
-      this.emailError = false;
-      this.emailErrorMessage = '';
-    }
-  }
-
+  ngAfterViewInit(): void {
  
-  validatePassword(): void {
-    if(!this.password) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password is required!';
-    } else if (this.password.length < 4) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password must be at least 4 characters!';    
-    } else {
-      this.passwordError = false;
-      this.passwordErrorMessage = '';
+  }
+
+  get username(): AbstractControl<any, any> | null {
+    return this.registerForm.get('name');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.registerForm.get('email');
+  }
+
+  get passwords(): FormGroup<any> {
+    return this.registerForm.get('passwords') as FormGroup;
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.passwords.get('password');
+  }
+
+  get rePassword(): AbstractControl<any, any> | null {
+    return this.passwords.get('rePassword');
+  }
+
+  get isUsernameValid(): boolean {
+    return this.username?.invalid && (this.username?.dirty || this.username?.touched) || false;
+  }
+
+  get isEmailValid(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get isPasswordsValid(): boolean {
+    return this.passwords?.invalid && (this.passwords?.dirty || this.passwords?.touched) || false;
+  }
+
+  get usernameErrorMessage(): string {
+    if (this.username?.errors?.['required']) {
+      return 'Username is required!';
     }
 
-    if (this.rePassword) {
-      this.validateRePassword();
+    if (this.username?.errors?.['minlength']) {
+      return 'Username should have at least 5 characters!';
+    }
+
+    return '';
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!';
+    }
+
+    if (this.email?.errors?.['pattern']) {
+      return 'Email is not valid!';
+    }
+
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 5 characters!';
+    }
+
+    if (this.password?.errors?.['pattern']) {
+      return 'Password is not valid!';
+    }
+
+    if (this.password?.errors?.['passwordMismatch']) {
+      return 'Passwords do not match!';
+    }
+
+    return '';
+  }
+
+  get rePasswordErrorMessage(): string {
+    if (this.rePassword?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.rePassword?.errors?.['minlength']) {
+      return 'Password must be at least 5 characters!';
+    }
+
+    if (this.rePassword?.errors?.['passwordMismatch']) {
+      return 'Passwords do not match!';
+    }
+
+    return '';
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const { name, email } = this.registerForm.value;
+      const { password, rePassword } = this.registerForm.value.passwords;
+
+      this.authService.register(name, email, password, rePassword).subscribe({
+          next: (user) => {
+            this.router.navigate(['/home']);
+          },
+          error: (err) => {
+            this.serverErrorMessage = err?.error?.error || 'Registration failed. Please try again.';
+            this.markFormGroupTouched();
+          }
+        });;
     }
   }
 
-  validateRePassword(): void {
-    if(!this.rePassword) {
-      this.rePasswordError = true;
-      this.rePasswordErrorMessage = 'Repeat Password is required!';
-    } else if (this.password !== this.rePassword) {
-      this.rePasswordError = true;
-      this.rePasswordErrorMessage = 'Repeat Password does not match password!';    
-    } else {
-      this.rePasswordError = false;
-      this.rePasswordErrorMessage = '';
-    }
-  }
-
-  isFormValid(): boolean {
-    return Boolean(this.name) && 
-      Boolean(this.email) && 
-      Boolean(this.password) && 
-      Boolean(this.rePassword) && 
-      !this.nameError && 
-      !this.emailError && 
-      !this.passwordError && 
-      !this.rePasswordError;
-  }
-
-onSubmit(): void {
-
-  this.validateName();
-  this.validateEmail();
-  this.validatePassword();
-  this.validateRePassword();
-
-  if (this.isFormValid()) {
-    this.authService.register(
-      this.name,
-      this.email,
-      this.password,
-      this.rePassword
-    ).subscribe({
-      next: (success: boolean) => {
-        console.log('Registration success:', success);
-        if (success) {
-          this.router.navigate(['/home']);
-        } else {
-          alert('Registration failed. Please try again.');
-        }
-      },
-      error: (err) => {
-        console.error('Registration error:', err);
-        alert('An error occurred during registration.');
+  private markFormGroupTouched(): void {
+    Object.keys(this.registerForm.controls).forEach(key => {
+      const control = this.registerForm.get(key);
+      if (control instanceof FormGroup) {
+        Object.keys(control.controls).forEach(nestedKey => {
+          const nestedControl = control.get(nestedKey)
+          nestedControl?.markAllAsTouched();
+        })
+      } else {
+        control?.markAsTouched();
       }
-    });
-  }
-}
-
-
-
-  private isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    return emailRegex.test(email);
+    })
   }
 
+  private passwordMatchValidator(passwordsControl: AbstractControl): ValidationErrors | null {
+    const password = passwordsControl.get('password');
+    const rePassword = passwordsControl.get('rePassword');
+
+    if (password && rePassword && password.value !== rePassword.value) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
 }
