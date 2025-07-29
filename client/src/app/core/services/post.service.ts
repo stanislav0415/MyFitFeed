831fe8f489fc88775750dd506d231df6f799cef4
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-07-29 17:18:15 +0300

    adding post service needs to be fixed

---
+++ client/src/app/core/services/post.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private readonly apiUrl = 'http://localhost:3000/posts';
+  constructor(private httpClient: HttpClient) {}
+
+  createPost(postData: Omit<Post, 'id' | 'createdAt'>): Observable<Post> {
+    return this.httpClient.post<Post>(`${this.apiUrl}/create`, postData, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  getPosts(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.apiUrl);
+  }
+
+  getPostById(id: string): Observable<Post> {
+    return this.httpClient.get<Post>(`${this.apiUrl}/${id}`);
+  }
+
+  updatePost(id: string, postData: Partial<Post>): Observable<Post> {
+    return this.httpClient.put<Post>(`${this.apiUrl}/${id}`, postData, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  deletePost(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
