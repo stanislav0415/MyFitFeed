77b0523633fe58273d372a5ff8617bbd8b0e0340
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-07-30 12:31:19 +0300

    fixed get started button

--- client/src/app/features/home/home.ts
+++ client/src/app/features/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service.js';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  selector: 'home', 
+  templateUrl: './home.html'
 })
 export class Home {
+  protected authService = inject(AuthService);
 
+  constructor(private router: Router) {}
+
+  handleGetStarted(): void {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/posts']);
+    } else {
+      this.router.navigate(['/register']);
+    }
+  }
 }
