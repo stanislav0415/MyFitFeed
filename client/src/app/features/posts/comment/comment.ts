c525c7fb98dd15ed01eaf4529a678d1bf7af8404
stanislav224 <149686416+stanislav0415@users.noreply.github.com>
2025-08-06 10:12:04 +0300

    adding comment comp

--- client/src/app/features/posts/comment/comment.ts
+++ client/src/app/features/posts/comment/comment.ts
@@ -1,22 +1,47 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { User } from '../../../models/user.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-comment',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './comment.html',
-  styleUrls: ['./comment.css']
 })
 export class Comment {
-  @Input() comments: { user: User, comment: string, createdAt: string }[] = [];
+  @Input() comments: any[] = [];
+  @Input() currentUser: any = null;
+  @Input() editedComment: { _id: string; comment: string } | null = null;
   @Input() newComment: string = '';
-  @Output() newCommentChange = new EventEmitter<string>();
-  @Output() addComment = new EventEmitter<void>();
 
-  onCommentChange(value: string) {
-    this.newCommentChange.emit(value);
-  }
+  @Output() addComment = new EventEmitter<void>();
+  @Output() deleteComment = new EventEmitter<string>();
+  @Output() editCommentStart = new EventEmitter<any>();
+  @Output() editCommentCancel = new EventEmitter<void>();
+  @Output() editCommentSave = new EventEmitter<void>();
+  @Output() newCommentChange = new EventEmitter<string>();
 
   onAddComment() {
     this.addComment.emit();
+  }
+
+  onDeleteComment(id: string) {
+    this.deleteComment.emit(id);
+  }
+
+  onStartEdit(comment: any) {
+    this.editCommentStart.emit(comment);
+  }
+
+  onCancelEdit() {
+    this.editCommentCancel.emit();
+  }
+
+  onSaveEditedComment() {
+    this.editCommentSave.emit();
+  }
+
+  onNewCommentChange(value: string) {
+    this.newCommentChange.emit(value);
   }
 }
